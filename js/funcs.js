c5617c5107922cfcf640a6a38bc2331dcce18bad
Júlia Vila <juliafvila@gmail.com>
2017-06-24 16:50:04 -0300

    teste "appPage" 2

--- js/funcs.js
+++ js/funcs.js
@@ -1,31 +1,3 @@
-function updateDepth(book, newPage) {
-
-	var page = book.turn('page'),
-		pages = book.turn('pages'),
-		depthWidth = 16*Math.min(1, page*2/pages);
-
-		newPage = newPage || page;
-
-	if (newPage>3)
-		$('.flipbook .p2 .depth').css({
-			width: depthWidth,
-			left: 20 - depthWidth
-		});
-	else
-		$('.flipbook .p2 .depth').css({width: 0});
-
-		depthWidth = 16*Math.min(1, (pages-page)*2/pages);
-
-	if (newPage<pages-3)
-		$('.flipbook .pPenultimo .depth').css({
-			width: depthWidth,
-			right: 20 - depthWidth
-		});
-	else
-		$('.flipbook .pPenultimo .depth').css({width: 0});
-
-}
-
 function loadPage(page) {
 
 	$.ajax({url: 'pages/page' + page + '.html'}).
@@ -52,4 +24,32 @@ function addPage(page, book) {
 		}
 
 	}
+}
+
+function updateDepth(book, newPage) {
+
+	var page = book.turn('page'),
+		pages = book.turn('pages'),
+		depthWidth = 16*Math.min(1, page*2/pages);
+
+		newPage = newPage || page;
+
+	if (newPage>3)
+		$('.flipbook .p2 .depth').css({
+			width: depthWidth,
+			left: 20 - depthWidth
+		});
+	else
+		$('.flipbook .p2 .depth').css({width: 0});
+
+		depthWidth = 16*Math.min(1, (pages-page)*2/pages);
+
+	if (newPage<pages-3)
+		$('.flipbook .pPenultimo .depth').css({
+			width: depthWidth,
+			right: 20 - depthWidth
+		});
+	else
+		$('.flipbook .pPenultimo .depth').css({width: 0});
+
 }
